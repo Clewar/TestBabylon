56c87108a3ca6c001fedf9284ee91a5f2ba1764e
Clewar <isaimonza99@gmail.com>
2020-07-02 15:44:15 -0500

    idk what im doing

--- camera.js
+++ camera.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("renderCanvas"); // Get the canvas element
 var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+var zommed = false;
 
 var dome, tickCount = -240, zoomLevel = 1;
 
@@ -9,12 +10,15 @@ var createScene = function () {
     var scene = new BABYLON.Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.ArcRotateCamera("arcR", Math.PI/2, Math.PI/2, 15, BABYLON.Vector3.Zero(), scene);
+	var arcCamera = new BABYLON.ArcRotateCamera("arcR", -Math.PI/2, Math.PI/2, 60, BABYLON.Vector3.Zero(), scene);	
+
+	var followCamera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(4, 40, -50), scene);
 
     // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-
-	camera.inputs.attached.mousewheel.detachControl(canvas);
+	//arcCamera.attachControl(canvas, true);
+	arcCamera.attachControl(canvas,true);
+	
+	//camera.inputs.attached.mousewheel.detachControl(canvas);
 	
 	// Domo
     dome = new BABYLON.VideoDome(
@@ -53,7 +57,7 @@ var createScene = function () {
 	// Tv
 
 	var ANote0 = BABYLON.MeshBuilder.CreateBox("ANote0", {width: 7.646700, height: 5.726200, depth: 0.100000 }, scene);
-	ANote0.position = new BABYLON.Vector3(4,2,40);
+	ANote0.position = new BABYLON.Vector3(4,40,60);
 	var mat = new BABYLON.StandardMaterial("ANote0Mat",scene);
 	mat.diffuseColor = new BABYLON.Color4(0, 0, 0, 1);
 	ANote0.material = mat;
@@ -62,14 +66,22 @@ var createScene = function () {
 			width: 7.3967, 
 			sideOrientation: BABYLON.Mesh.BACKSIDE
 	};
+
+	// Creaste follow camera for videoplaying
+	/*var followCamera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(4, 40, 50), scene, ANote0);
+	followCamera.radius = 10;
+	followCamera.cameraAcceleration = 0.15;*/
+	
+
 	var ANote0Video = BABYLON.MeshBuilder.CreatePlane("plane", planeOpts, scene);
-	var vidPos = (new BABYLON.Vector3(0,0,0.1)).addInPlace(ANote0.position);
+	var vidPos = new BABYLON.Vector3(4,40,59.9);
     ANote0Video.position = vidPos;
 	var ANote0VideoMat = new BABYLON.StandardMaterial("m", scene);
-	var ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex","textures/babylonjs.mp4", scene);
+	var ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex","Media/babylonjs.mp4", scene);
 	ANote0VideoMat.diffuseTexture = ANote0VideoVidTex;
 	ANote0VideoMat.roughness = 1;
 	ANote0VideoMat.emissiveColor = new BABYLON.Color3.White();
+	//ANote0VideoMat.ambientColor = new BABYLON.Color3.White();
 	ANote0Video.material = ANote0VideoMat;
 	scene.onPointerObservable.add(function(evt){
 			if(evt.pickInfo.pickedMesh === ANote0Video){
@@ -78,7 +90,8 @@ var createScene = function () {
 						ANote0VideoVidTex.video.play();
 					else
 						ANote0VideoVidTex.video.pause();
-                    console.log(ANote0VideoVidTex.video.paused?"paused":"playing");
+					console.log(ANote0VideoVidTex.video.paused?"paused":"playing");
+				
 			}
 	}, BABYLON.PointerEventTypes.POINTERPICK);
 
